2c05ed503dba7591a35d3263a67a4160d85a29ae
Nathan Yan <nathan_yan_alves@hotmail.com>
2025-01-11 00:05:40 -0300

    Add utility functions, components, and global styles for theming and languages

---
+++ src/components/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Jost, Playfair_Display } from "next/font/google";
+
+export const jost = Jost({
+	subsets: ["latin"],
+});
+
+export const playfairDisplay = Playfair_Display({
+	subsets: ["latin"],
+});
