059c10dfb22bd78c46148385a7c662c96be71ce7
Nathan Yan <nathan_yan_alves@hotmail.com>
2025-01-12 22:54:39 -0300

    Enhance Contacts and WhyHireMe components with improved typography and responsive design for better readability and user experience

--- src/components/contacts.tsx
+++ src/components/contacts.tsx
@@ -17,33 +17,33 @@ export const Contacts = () => {
 		<div className="w-full h-screen">
 			<section className="max-w-7xl mx-auto h-full px-5 border-t border-t-gray-600 pt-[14px]">
 				<div className="h-full flex flex-col justify-evenly">
-					<h1 className="text-5xl font-bold text-center">
+					<h1 className="text-6xl font-bold text-center">
 						{language === "pt-BR" ? "Contatos" : "Contacts"}
 					</h1>
 					<div className="flex items-center justify-around mt-5 h-[350px]">
 						<div className="flex items-center  gap-2  h-3/5 px-4">
-							<LinkedinIcon fill="#0b65c3" width={64} height={64} />
+							<LinkedinIcon fill="#0b65c3" width={80} height={80} />
 							<Link
 								href={"https://www.linkedin.com/in/nathan-yan-alves/"}
 								target="_blank"
 							>
-								<span className="text-3xl">Nathan Yan</span>
+								<span className="text-5xl">Nathan Yan</span>
 							</Link>
 						</div>
 						<div className="flex items-center gap-2  h-3/5 px-4">
 							{theme === "dark" ? (
-								<GitHubWhiteIcon width={64} height={64} />
+								<GitHubWhiteIcon width={80} height={80} />
 							) : (
-								<GitHubBlackIcon width={64} height={64} />
+								<GitHubBlackIcon width={80} height={80} />
 							)}
 							<Link href={"https://github.com/NathanYanDev"} target="_blank">
-								<span className="text-3xl">Nathan Yan</span>
+								<span className="text-5xl">Nathan Yan</span>
 							</Link>
 						</div>
 						<div className="flex items-center  gap-2 h-3/5 px-4">
-							<Mail width={64} height={64} />
+							<Mail width={80} height={80} />
 							<Link href={"mailto:yan_dev@zohomail.com"} target="_blank">
-								<span className="text-3xl">Yan Dev</span>
+								<span className="text-5xl">Yan Dev</span>
 							</Link>
 						</div>
 					</div>
--- src/components/whyHireMe.tsx
+++ src/components/whyHireMe.tsx
@@ -16,60 +16,64 @@ export const WhyHireMe = () => {
 		<div className="w-full h-screen bg-[#3183FF]" id="whyHireMe">
 			<section className="max-w-7xl mx-auto h-full px-5 border-t border-t-gray-600 pt-[14px]">
 				<div className="h-full flex flex-col items-center justify-evenly">
-					<h1 className="text-5xl font-bold text-white">
+					<h1 className="text-6xl font-bold text-white">
 						{language === "pt-BR" ? "Porque me contratar?" : "Why hire me?"}
 					</h1>
 					<div className="flex items-center gap-5 mt-5 h-[350px]">
 						<Card className="h-[250px] hover:scale-110">
 							<CardHeader>
-								<CardTitle className="flex items-center gap-2">
+								<CardTitle className="flex items-center gap-2 text-lg">
 									<Flag />
-									Resolução de problemas
+									{language === "pt-BR"
+										? "Resolução de problemas"
+										: "Problem Solving"}
 								</CardTitle>
 							</CardHeader>
 							<CardContent>
-								Resiliência para resolver problemas simples, do cotidiano, e
-								problemas complexos. Com muita calma e paciência, monto uma
-								linha de raciocínio para uma melhor abordagem.
+								{language === "pt-BR"
+									? "Resiliência para resolver problemas simples, do cotidiano, e problemas complexos. Com muita calma e paciência, monto uma linha de raciocínio para uma melhor abordagem."
+									: "Resilience to solve simple, everyday problems, and complex ones. With a lot of calm and patience, I build a line of reasoning for a better approach."}
 							</CardContent>
 						</Card>
 						<Card className="h-[250px] hover:scale-110">
 							<CardHeader>
-								<CardTitle className="flex items-center gap-2">
+								<CardTitle className="flex items-center gap-2 text-lg">
 									<Gem />
-									Adaptabilidade
+									{language === "pt-BR" ? "Adaptabilidade" : "Adaptability"}
 								</CardTitle>
 							</CardHeader>
 							<CardContent>
-								Sempre pronto para aprender e me ajustar às novas tecnologias do
-								mercado. Com foco em soluções inovadoras, enfrento desafios com
-								flexibilidade e criatividade.
+								{language === "pt-BR"
+									? "Sempre pronto para aprender e me ajustar às novas tecnologias do mercado. Com foco em soluções inovadoras, enfrento desafios com flexibilidade e criatividade."
+									: "Always ready to learn and adapt to new market technologies. With a focus on innovative solutions, I face challenges with flexibility and creativity."}
 							</CardContent>
 						</Card>
 						<Card className="h-[250px] hover:scale-110">
 							<CardHeader>
-								<CardTitle className="flex items-center gap-2">
+								<CardTitle className="flex items-center gap-2 text-lg">
 									<Lightbulb />
-									Aprendizagem contínua
+									{language === "pt-BR"
+										? "Aprendizagem contínua"
+										: "Continuous learning"}
 								</CardTitle>
 							</CardHeader>
 							<CardContent>
-								Estou constantemente realizando cursos online e participando de
-								workshops para me manter atualizado com as últimas tendências e
-								tecnologias do setor.
+								{language === "pt-BR"
+									? "Estou constantemente realizando cursos online e participando de workshops para me manter atualizado com as últimas tendências e tecnologias do setor."
+									: "I am constantly taking online courses and participating in workshops to stay updated with the latest trends and technologies in the industry."}
 							</CardContent>
 						</Card>
 						<Card className="h-[250px] hover:scale-110">
 							<CardHeader>
-								<CardTitle className="flex items-center gap-2">
+								<CardTitle className="flex items-center gap-2 text-lg">
 									<Handshake />
-									Trabalho em equipe
+									{language === "pt-BR" ? "Trabalho em equipe" : "Teamwork"}
 								</CardTitle>
 							</CardHeader>
 							<CardContent>
-								Tenho experiência em trabalho em equipe, com boa comunicação e
-								habilidade para colaborar com os colegas, visando entregar
-								soluções eficientes.
+								{language === "pt-BR"
+									? "Tenho experiência em trabalho em equipe, com boa comunicação e habilidade para colaborar com os colegas, visando entregar soluções eficientes."
+									: "I have experience in teamwork, with good communication skills and the ability to collaborate with colleagues to deliver efficient solutions."}
 							</CardContent>
 						</Card>
 					</div>
